b1252d4947c66c56dd96e9609198100735f39aed
zhongxx <zhongxx@umich.edu>
2022-03-06 12:21:03 -0500

    Add MapTable Module

---
+++ verilog/MapTable.sv
@@ -0,0 +1,82 @@
+module MapTable (
+        //INPUT
+        input logic clock,
+        input logic reset,
+
+        input logic [$clog2(`ROB_SIZE+1)-1:0] ROB_idx, //rd tag from ROB in dispatch stage: start from 1
+        input logic [$clog2(`REG_SIZE)-1:0] rd_dispatch, // dest reg to be set in dispatch stage
+
+        input logic [$clog2(`ROB_SIZE+1)-1:0] CDB_tag, //rd tag from CDB in complete stage
+
+        input logic [$clog2(`REG_SIZE)-1:0] rs1_dispatch, //check rs1 tag for RS
+        input logic [$clog2(`REG_SIZE)-1:0] rs2_dispatch, //check rs2 tag for RS
+
+        input logic [$clog2(`REG_SIZE)-1:0] rd_retire, // rd idx of the head entry in ROB
+        input logic clear, //whether to clear tag in retire stage
+
+        input logic flush, //for brach or exception 
+
+        //OUTPUT
+        output logic [$clog2(`ROB_SIZE+1)-1:0] rs1_tag,
+        output logic [$clog2(`ROB_SIZE+1)-1:0] rs2_tag, 
+        output logic rs1_ready, //whether valid in ROB
+        output logic rs2_ready
+    );
+
+    //MapTable
+    logic [$clog2(`ROB_SIZE+1)-1:0] Tag [`REG_SIZE-1:0];
+    logic [`REG_SIZE-1:0] ready_in_ROB;
+
+    //To avoid multiple drive
+    logic [$clog2(`ROB_SIZE+1)-1:0] Tag_next [`REG_SIZE-1:0];
+    logic [`REG_SIZE-1:0] ready_in_ROB_next;
+
+    always_comb begin
+        if (flush) begin
+            Tag_next = '{default:32'h0000_0000_0000_0000};
+            ready_in_ROB_next = 0;
+        end
+        else begin
+            Tag_next = Tag;
+            ready_in_ROB_next = ready_in_ROB;
+
+            //clear Tag in retire stage
+            if (clear) begin
+                Tag_next[rd_retire] = 0;
+                ready_in_ROB_next[rd_retire] = 0;
+            end
+
+            //set ready bit in complete stage
+            for (int i = 0; i < `REG_SIZE; i++)  begin
+                if (Tag[i] == CDB_tag) begin
+                    ready_in_ROB_next[i] = 1'b1;
+                    //break; 
+                end
+            end
+            //set rd tag in dispatch stage
+            Tag_next[rd_dispatch] = ROB_idx;
+            end
+    end
+
+    
+    //MapTable output in dispatch
+    assign rs1_tag = Tag_next[rs1_dispatch]; //bypassing from retire/complete to dispatch
+    assign rs2_tag = Tag_next[rs2_dispatch];
+    assign rs1_ready = ready_in_ROB_next[rs1_dispatch];
+    assign rs2_ready = ready_in_ROB_next[rs2_dispatch];
+
+    // synopsys sync_set_reset "reset"
+    always_ff @(posedge clock) begin
+        if (reset | flush) begin 
+            //All from reg file
+            Tag <= `SD '{default:32'h0000_0000_0000_0000};//need to change to `SD
+            ready_in_ROB <= `SD 0;
+        end
+        else begin
+            //update Maptable
+            Tag <= `SD Tag_next;//need to change to `SD
+            ready_in_ROB <= `SD ready_in_ROB_next;
+        end
+    end
+
+endmodule
---
+++ verilog/test_MapTable.sv
@@ -0,0 +1,196 @@
+module testbench;
+
+    logic clock, reset, clear, flush;
+    logic [$clog2(`REG_SIZE)-1:0] rs1_dispatch, rs2_dispatch, rd_retire, rd_dispatch;
+    logic [$clog2(`ROB_SIZE+1)-1:0] rs1_tag, rs2_tag, ROB_idx, CDB_tag;
+    logic rs1_ready, rs2_ready;
+
+    MapTable test_map (.clock(clock),
+    .reset(reset),
+    .ROB_idx(ROB_idx),
+    .rd_dispatch(rd_dispatch),
+    .CDB_tag(CDB_tag),
+    .rs1_dispatch(rs1_dispatch),
+    .rs2_dispatch(rs2_dispatch),
+    .rd_retire(rd_retire),
+    .clear(clear),
+    .flush(flush),
+
+    .rs1_tag(rs1_tag),
+    .rs2_tag(rs2_tag),
+    .rs1_ready(rs1_ready),
+    .rs2_ready(rs2_ready)
+    );
+
+    always #5 clock = ~clock;
+    task exit_on_error;
+		begin
+			#1;
+			$display("@@@Failed at time %f", $time);
+			$finish;
+		end
+	endtask
+
+    initial begin
+        clock = 0;
+
+        /**RESET TEST**/
+        reset = 1;
+        flush = 0;
+        @(negedge clock);
+
+        //all tag = 0 and not ready
+        for(int i=0; i<`REG_SIZE; i++) begin
+            rs1_dispatch = i;
+            if (rs1_tag != 0 | rs1_ready) begin
+                $display("@@@Reset Error");
+                exit_on_error;
+            end
+        end
+
+        reset = 0;
+
+        /**Dispatch TEST**/
+        rd_dispatch = 15;
+        ROB_idx = 1;
+        @(negedge clock);
+        rd_dispatch = 11;
+        ROB_idx = 2;
+        @(negedge clock);
+
+        #1
+        for(int i=0; i<`REG_SIZE; i++) begin
+            rs1_dispatch = i;
+            if (rs1_dispatch == 11) begin
+                if (rs1_tag != 2 | rs1_ready) begin 
+                    $display("@@@Dispatch Error");
+                    exit_on_error;
+                end
+            end
+            else if (rs1_dispatch == 15) begin
+                if (rs1_tag != 1 | rs1_ready) begin 
+                    $display("@@@Dispatch Error");
+                    exit_on_error;
+                end
+            end
+            else begin
+                if (rs1_tag != 0 | rs1_ready) begin
+                    $display("@@@Dispatch Error");
+                    exit_on_error;
+                end
+            end
+        end 
+        
+
+        /**COMPLETE TEST**/
+        rd_dispatch = 31;
+        ROB_idx = 3;
+        @(negedge clock);
+        rd_dispatch = 7;
+        ROB_idx = 4;
+        @(negedge clock);
+
+        //ROB#2 is completed
+        CDB_tag = 2;
+        rs2_dispatch = 11;
+        #1
+        if (rs2_tag != 2 | ~rs2_ready) begin
+            $display("@@@Complete Error 1");
+            exit_on_error;
+        end
+        @(negedge clock);
+
+        //ROB#3 is completed
+        CDB_tag = 3;
+        rs1_dispatch = 31;
+        #1
+        if (rs1_tag != 3 | ~rs1_ready) begin
+            $display("@@@Complete Error 2");
+            exit_on_error;
+        end
+        @(negedge clock);
+        
+        //ROB#5 is completed +  ROB#5 is not in MapTable
+        CDB_tag = 5;
+        rs2_dispatch = 7;
+        #1
+        if (rs2_tag != 4 | rs2_ready) begin
+            $display("@@@Complete Error 3");
+            exit_on_error;
+        end
+
+        /**RETIRE TESET**/
+        //retire reg[31]
+        clear = 1;
+        rd_retire = 31;
+        rs1_dispatch = 31;
+        rs2_dispatch = 11;
+        @(negedge clock);
+        if (rs1_tag !=0 | rs1_ready | rs2_tag != 2| ~rs2_ready) begin
+            $display("@@@Retire Error 1");
+            exit_on_error;
+        end
+        @(negedge clock);
+
+        clear = 0;
+        @(negedge clock);
+        //no instruction retire
+        #1
+        for(int i=0; i<`REG_SIZE; i++) begin
+            rs1_dispatch = i;
+            if (rs1_dispatch == 7) begin //dispatched
+                if (rs1_tag != 4 | rs1_ready) begin 
+                    $display("@@@Retire Error");
+                    exit_on_error;
+                end
+            end
+            else if (rs1_dispatch == 11) begin // completed
+                if (rs1_tag != 2 | ~rs1_ready) begin 
+                    $display("@@@Retire Error");
+                    exit_on_error;
+                end
+            end
+            else if (rs1_dispatch == 15) begin //dispatched
+                if (rs1_tag != 1 | rs1_ready) begin 
+                    $display("@@@Retire Error");
+                    exit_on_error;
+                end
+            end
+            else begin
+                if (rs1_tag != 0 | rs1_ready) begin
+                    $display("@@@Retire Error");
+                    exit_on_error;
+                end
+            end
+        end
+
+        //retire reg[11]
+        clear = 1;
+        rd_retire = 11;
+        @(negedge clock);
+        if (rs1_tag !=0 | rs1_ready | rs2_tag != 0| rs2_ready) begin
+            $display("@@@Retire Error 3");
+            exit_on_error;
+        end
+
+        /**FLUSH TEST**/
+        flush = 1;
+        reset = 0;
+        @(negedge clock);
+
+        //all tag = 0 and not ready
+        for(int i=0; i<`REG_SIZE; i++) begin
+            rs1_dispatch = i;
+            if (rs1_tag != 0 | rs1_ready) begin
+                $display("@@@Flush Error");
+                exit_on_error;
+            end
+        end
+
+     $display("@@@Passed");
+     $finish;
+
+    end
+    
+
+endmodule
