b96957c75e7bac78d8b3370df08a1b283b33e044
Ramchandra K Apte <ramkapte@caen-vnc-mi12.engin.umich.edu>
2022-03-30 10:50:06 -0400

    improve visuals of debugger

--- testbench/visual.py
+++ testbench/visual.py
@@ -24,7 +24,7 @@ for line in out:
     data[cycle].lines.append(line)
     line = line[match.end() :]
     if line.startswith("registers"):
-        data[cycle].registers.extend([x] for x in line.split(", ")[:-1])
+        data[cycle].registers.extend([x.split(" = ")[1]] for x in line.split(", ")[:-1])
         continue
     entry_match = re.match(r"rob_entries\[\s+(\d+)\] = '{(.*)}", line)
     if not entry_match:
@@ -34,34 +34,37 @@ for line in out:
     idx = int(idx)
     for kv in entry.split(","):
         key, value = kv.split(":")
+        value = value[2:]
+        #if key == "value":
+            #value = value.rjust(10, " ")
         data[cycle].entries[key].append(value)
 
-OPTS = dict(tablefmt="simple")
+#tabulate.PRESERVE_WHITESPACE = True
+
+OPTS = dict(tablefmt = "fancy_grid")
 
 tabulate = functools.partial(tabulate.tabulate, **OPTS)
-
 
 def main(screen):
     cycle = 0
     while True:
         screen.clear()
-        screen.addstr(f"Cycle {cycle}\n")
+        screen.addstr(f"Cycle {cycle}"+" "*10 +"Press q to quit" + "\n")
         cnt = 0
         for line in data[cycle].lines:
+            if cnt >= 0:
+                break
             screen.addstr(line)
             cnt += 1
-            if cnt > 5:
-                break
         NUM_REG = 10
-        screen.addstr(
-            tabulate(
-                data[cycle].registers[:NUM_REG], showindex="always", headers="firstrow"
-            )
-            + "\n"
-        )
-        screen.addstr(
-            tabulate(data[cycle].entries, showindex="always", headers="keys") + "\n"
-        )
+        table1 = tabulate(data[cycle].registers[:NUM_REG], showindex="always", headers="firstrow").splitlines()
+        table2 = tabulate(data[cycle].entries, showindex="always", headers="keys").splitlines()
+        for idx in range(max(len(table1), len(table2))):
+            max_len = max(map(len, table1))
+            row1 = (table1[idx] if idx < len(table1) else "")
+            row2 = (table2[idx] if idx < len(table2) else "")
+            row1 = row1.ljust(max_len, " ")
+            screen.addstr(row1+ " "*10 + row2 + "\n")
         screen.refresh()
         offset = {"KEY_RIGHT": 1, "KEY_LEFT": -1}
         key = screen.getkey()
@@ -71,7 +74,7 @@ def main(screen):
         cycle += offset[key]
 
 
-# print(data[0].entries)
+#print(data[0].entries)
 
 
 curses.wrapper(main)
